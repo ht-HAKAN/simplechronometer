ac8076e876be011a9e4a880b6e69a217999caf3f
Hakan Tuna <ht.tunahakan@gmail.com>
2025-03-22 15:34:16 +0300

    design final update.

--- simplechronometer/chronometer.Designer.cs
+++ simplechronometer/chronometer.Designer.cs
@@ -33,10 +33,10 @@
             // time
             // 
             this.time.AutoSize = true;
-            this.time.BackColor = System.Drawing.Color.DimGray;
+            this.time.BackColor = System.Drawing.Color.Black;
             this.time.Font = new System.Drawing.Font("Unispace", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.time.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.time.Location = new System.Drawing.Point(-8, 40);
+            this.time.Location = new System.Drawing.Point(-8, 9);
             this.time.Name = "time";
             this.time.Size = new System.Drawing.Size(698, 115);
             this.time.TabIndex = 0;
@@ -45,40 +45,49 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 194);
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Sitka Banner", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 155);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 70);
+            this.button1.Size = new System.Drawing.Size(160, 60);
             this.button1.TabIndex = 1;
             this.button1.Text = "Başlat";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(257, 194);
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Sitka Banner", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(254, 155);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 70);
+            this.button2.Size = new System.Drawing.Size(160, 60);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(504, 194);
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Sitka Banner", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(504, 155);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(170, 70);
+            this.button3.Size = new System.Drawing.Size(160, 60);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Text = "Sıfırla";
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // chronometer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(686, 270);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(686, 233);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
