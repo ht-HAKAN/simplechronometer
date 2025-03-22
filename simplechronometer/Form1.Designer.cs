7ec5633d18cd0f3cc29f98e1319d8f41fd254725
Hakan Tuna <ht.tunahakan@gmail.com>
2025-03-22 15:01:51 +0300

    design update

--- simplechronometer/Form1.Designer.cs
+++ simplechronometer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace simplechronometer
 {
-    partial class Form1
+    partial class Chronometer
     {
         /// <summary>
         /// Required designer variable.
@@ -28,36 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chronometer));
+            this.timer = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // timer
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(323, 158);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.timer.Location = new System.Drawing.Point(156, 99);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(385, 76);
+            this.timer.TabIndex = 0;
+            this.timer.Text = "00:00:00:00";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(306, 321);
+            this.button1.Location = new System.Drawing.Point(12, 262);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(170, 70);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Başlat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(407, 321);
+            this.button2.Location = new System.Drawing.Point(257, 262);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(170, 70);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,25 +67,27 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(511, 321);
+            this.button3.Location = new System.Drawing.Point(504, 262);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(170, 70);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // Form1
+            // Chronometer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(686, 357);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.timer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Chronometer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chronometer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,7 +95,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timer;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- simplechronometer/Form1.cs
+++ simplechronometer/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace simplechronometer
 {
-    public partial class Form1: Form
+    public partial class Chronometer: Form
     {
-        public Form1()
+        public Chronometer()
         {
             InitializeComponent();
         }
